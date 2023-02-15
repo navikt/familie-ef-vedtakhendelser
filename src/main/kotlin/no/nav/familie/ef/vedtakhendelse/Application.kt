1300d0e7f9addb885cce3d0a3afd9b0bcc43465e
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-02-15 11:26:45 +0100

    Linterendringer

--- src/main/kotlin/no/nav/familie/ef/vedtakhendelse/Application.kt
+++ src/main/kotlin/no/nav/familie/ef/vedtakhendelse/Application.kt
@@ -2,7 +2,6 @@ package no.nav.familie.ef.vedtakhendelse
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 
 @SpringBootApplication
 class Application
--- src/main/kotlin/no/nav/familie/ef/vedtakhendelse/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/vedtakhendelse/config/ApplicationConfig.kt
@@ -3,23 +3,12 @@ package no.nav.familie.ef.vedtakhendelse.config
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import no.nav.familie.kafka.KafkaErrorHandler
 import no.nav.familie.kontrakter.felles.objectMapper
-import no.nav.familie.log.filter.LogFilter
-import no.nav.familie.log.filter.RequestTimeFilter
-import no.nav.security.token.support.client.core.http.OAuth2HttpClient
-import no.nav.security.token.support.client.spring.oauth2.DefaultOAuth2HttpClient
-import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.web.client.RestTemplate
-import java.time.Duration
-import java.time.temporal.ChronoUnit
 
 @SpringBootConfiguration
 @ConfigurationPropertiesScan("no.nav.familie.ef.vedtakhendelse")
@@ -35,5 +24,4 @@ class ApplicationConfig {
     @Bean
     @Primary
     fun objectMapper() = objectMapper
-
 }
